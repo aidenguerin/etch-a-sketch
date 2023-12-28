44d49bec7ea5a604cafbc78d2a7f07c49ff1c311
Aiden Guerin <90363998+aidenguerin@users.noreply.github.com>
2023-12-28 22:59:17 +1100

    Added functionality for canvas resizing

--- scripts.js
+++ scripts.js
@@ -1,26 +1,47 @@
 // DEFAULT GRID SIZE
-let GRID_HEIGHT = 16;
-let GRID_WIDTH = 16;
+let canvas_height = 16;
+let canvas_width = 16;
 
-// DEFINE GRID
-const GRID = document.querySelector(".grid");
+// Generate default canvas
+const CANVAS = document.querySelector(".canvas")
+renderCanvas(canvas_height, canvas_width)
 
-for (let rowNum = 0; rowNum < GRID_HEIGHT; rowNum++) {
-    const row = document.createElement("div");
-    row.classList.add("row");
+// MENU BUTTONS
+const inputCanvasHeight = document.querySelector("#input-canvas-height");
+const inputCanvasWidth = document.querySelector("#input-canvas-width");
 
-    for (let colNum = 0; colNum < GRID_WIDTH; colNum++) {
-        const gridSquare = document.createElement("div");
-        gridSquare.classList.add("grid-square");
-        gridSquare.textContent = `${rowNum},${colNum}`;
-        
-         
-        gridSquare.addEventListener('mouseover', () => changeColor(gridSquare));
-        row.appendChild(gridSquare);
+const buttonAdjustCanvas = document.querySelector("#button-adjust-canvas");
+buttonAdjustCanvas.addEventListener("click", () => updateCanvasSize())
+
+
+function renderCanvas(canvas_height, canvas_width) {
+    for (let rowNum = 0; rowNum < canvas_height; rowNum++) {
+        const row = document.createElement("div");
+        row.classList.add("row");
+
+        for (let colNum = 0; colNum < canvas_width; colNum++) {
+            const gridSquare = document.createElement("div");
+            gridSquare.classList.add("grid-square");
+
+            gridSquare.addEventListener("mouseover", () => changeColor(gridSquare));
+            row.appendChild(gridSquare);
+        }
+        CANVAS.appendChild(row);
     }
-    GRID.appendChild(row); 
+}
+
+function updateCanvasSize() {
+    while (CANVAS.firstChild) {
+        CANVAS.removeChild(CANVAS.firstChild)
+    }
+
+    canvas_height = inputCanvasHeight.value;
+    canvas_width = inputCanvasWidth.value;
+
+    renderCanvas(canvas_height, canvas_width)
 }
 
 function changeColor(gridSquare) {
-    gridSquare.style.backgroundColor = 'red';
+    gridSquare.style.backgroundColor = "grey";
 }
+
